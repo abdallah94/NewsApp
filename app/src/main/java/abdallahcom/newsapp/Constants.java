a67353df1cae0936cf4cd7dfadc078010c1649bb
basharqassis <basharqassis9@hotmail.com>
2016-07-10 12:49:45 +0300

    Added url string of news source to constants

--- app/src/main/java/abdallahcom/newsapp/Constants.java
+++ app/src/main/java/abdallahcom/newsapp/Constants.java
@@ -4,7 +4,7 @@ package abdallahcom.newsapp;
  * Created by Bashar on 7/10/2016.
  */
 public class Constants {
-    public static final String ns = null;
+    public static final String NS = null;
     public static final String TAG = "NewsApp";
-    public static final String url = "http://feeds.bbci.co.uk/arabic/rss.xml";
+    public static final String URL = "http://feeds.bbci.co.uk/arabic/rss.xml";
 }
