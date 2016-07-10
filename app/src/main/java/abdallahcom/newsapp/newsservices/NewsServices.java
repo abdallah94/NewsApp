0fe7984e3fcbe5cb060dc3b5cd8656b18607ceb4
basharqassis <basharqassis9@hotmail.com>
2016-07-10 12:20:13 +0300

    Added NewsServices and NewsArticles

---
+++ app/src/main/java/abdallahcom/newsapp/newsservices/NewsServices.java
@@ -0,0 +1,159 @@
+package abdallahcom.newsapp.newsservices;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.Log;
+import android.util.Xml;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+import abdallahcom.newsapp.utilities.NewsArticle;
+
+/**
+ * Created by Bashar on 7/10/2016.
+ */
+public class NewsServices {
+    private static final String ns = null;
+
+    public static Bitmap getBitmapFromURL(String src) {
+        try {
+            URL url = new URL(src);
+            HttpURLConnection connection = (HttpURLConnection) url
+                    .openConnection();
+            connection.setDoInput(true);
+            connection.connect();
+            InputStream input = connection.getInputStream();
+            Bitmap myBitmap = BitmapFactory.decodeStream(input);
+
+            return myBitmap;
+        } catch (IOException e) {
+            e.printStackTrace();
+            Log.e("getBmpFromUrl error: ", e.getMessage().toString());
+            return null;
+        }
+    }
+
+    public List parse(InputStream in) throws XmlPullParserException, IOException {
+        try {
+            XmlPullParser parser = Xml.newPullParser();
+            parser.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES, false);
+            parser.setInput(in, null);
+            parser.nextTag();
+            return readFeed(parser);
+        } finally {
+            in.close();
+        }
+    }
+
+    private List readFeed(XmlPullParser parser) throws XmlPullParserException, IOException {
+        List entries = new ArrayList();
+
+        parser.require(XmlPullParser.START_TAG, ns, "feed");
+        while (parser.next() != XmlPullParser.END_TAG) {
+            if (parser.getEventType() != XmlPullParser.START_TAG) {
+                continue;
+            }
+            String name = parser.getName();
+            // Starts by looking for the entry tag
+            if (name.equals("item")) {
+                entries.add(readEntry(parser));
+            } else {
+                skip(parser);
+            }
+        }
+        return entries;
+    }
+
+    private void skip(XmlPullParser parser) {
+
+    }
+
+    private NewsArticle readEntry(XmlPullParser parser) throws XmlPullParserException, IOException {
+        parser.require(XmlPullParser.START_TAG, ns, "item");
+        String title = null;
+        String description = null;
+        String link = null;
+        String pubDate = null;
+        Bitmap image = null;
+        while (parser.next() != XmlPullParser.END_TAG) {
+            if (parser.getEventType() != XmlPullParser.START_TAG) {
+                continue;
+            }
+            String name = parser.getName();
+            if (name.equals("title")) {
+                title = readTitle(parser);
+            } else if (name.equals("description")) {
+                description = readSummary(parser);
+            } else if (name.equals("link")) {
+                link = readLink(parser);
+            } else if (name.equals("pubDate")) {
+                pubDate = readPubDate(parser);
+            } else if (name.equals("media:thumbnail")) {
+                image = readImage(parser);
+            } else {
+                skip(parser);
+            }
+        }
+        return new NewsArticle(title, description, link, pubDate, image);
+    }
+
+    private Bitmap readImage(XmlPullParser parser) throws IOException, XmlPullParserException {
+        String url = "";
+        parser.require(XmlPullParser.START_TAG, ns, "media:thumbnail");
+        url = parser.getAttributeValue(null, "url");
+        parser.require(XmlPullParser.END_TAG, ns, "media:thumbnail");
+        return getBitmapFromURL(url);
+    }
+
+    // Processes title tags in the feed.
+    private String readTitle(XmlPullParser parser) throws IOException, XmlPullParserException {
+        parser.require(XmlPullParser.START_TAG, ns, "title");
+        String title = readText(parser);
+        parser.require(XmlPullParser.END_TAG, ns, "title");
+        return title;
+    }
+
+    // Processes summary tags in the feed.
+    private String readSummary(XmlPullParser parser) throws IOException, XmlPullParserException {
+        parser.require(XmlPullParser.START_TAG, ns, "description");
+        String description = readText(parser);
+        parser.require(XmlPullParser.END_TAG, ns, "description");
+        return description;
+    }
+
+    // Processes link tags in the feed.
+    private String readLink(XmlPullParser parser) throws IOException, XmlPullParserException {
+        parser.require(XmlPullParser.START_TAG, ns, "link");
+        String link = readText(parser);
+        parser.require(XmlPullParser.END_TAG, ns, "link");
+        return link;
+    }
+
+    // Processes pubDate tags in the feed.
+    private String readPubDate(XmlPullParser parser) throws IOException, XmlPullParserException {
+        parser.require(XmlPullParser.START_TAG, ns, "pubDate");
+        String pubDate = readText(parser);
+        parser.require(XmlPullParser.END_TAG, ns, "pubDate");
+        return pubDate;
+    }
+
+
+    // For the tags title and summary, extracts their text values.
+    private String readText(XmlPullParser parser) throws IOException, XmlPullParserException {
+        String result = "";
+        if (parser.next() == XmlPullParser.TEXT) {
+            result = parser.getText();
+            parser.nextTag();
+        }
+        return result;
+    }
+
+}
